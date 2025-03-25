51fa5968b593333af85091ae0c5a2dbec271bb98
Nathan <nathanmtetwa@gmail.com>
2025-03-25 13:34:09 +0200

    Refactor Postcard component and styles; update title and layout

--- react-card-exercise/src/App.jsx
+++ react-card-exercise/src/App.jsx
@@ -1,27 +1,27 @@
-import { Post } from './components/post';
-import icy_mount from ' ./assets/icy_mount';
+import Post from './components/post';
+import icy_mount from './assets/icy_mount.png';
 
 
 function App(){
 
-const post = {
-    image: icy_mount,
-    title: 'Title',
-    content: 'Content',
-    read: 10,
-    views: 20,
-    comments: 30,
-    relativetime: '1 hour ago'
+        const post = {
+            image: icy_mount,
+            title: 'Post One',
+            content: 'lorem ipsum dolor sit amet lore ipsum dolor sit amet lore ipsum dolor sit amet lore ipsum dolor sit amet',
+            read: 102,
+            views: 5132,
+            comments: 30,
+            relativetime: '1 hour ago'
+        }
+
+    return(
+        <div>
+            <Post post={post}/>
+        </div>
+    )
+
+
+
 }
 
-return(
-    <div>
-        <Post post={post}/>
-    </div>
-)
-
-
-
-}
-
-export { App };
+export default  App ;
--- react-card-exercise/src/components/post.jsx
+++ react-card-exercise/src/components/post.jsx
@@ -1,19 +1,31 @@
-function Post ({ post }) {
-  return (
-    <div className="post">
-        <img src={post.image} alt={post.title} className="post-image"></img>
-        <h3  className="post-time">{post.relativetime}</h3>
-        <h2 className="post-title">{post.title}</h2>
-        <p className="post-content">{post.content}</p>
-        <div>
-        <span>{post.read} Read</span>
-            <span>{post.views} Views</span>
-            <span >{post.comments} Comments</span>
-        </div>
+import React from 'react';
+import '../style.css';
 
-      
+function Post({ post }) {
+  return (
+    <div className="card">
+      <img className="image" src={post.image} alt={post.title} />
+      <div className="content">
+        <h3 className="date">{post.relativetime}</h3>
+        <h2 className="title">{post.title}</h2>
+        <p className="description">{post.content}</p>
+      </div>
+      <div className="footer">
+        <div className="footerItem">
+          <p className="footerValue">{post.read}</p>
+          <p className="footerLabel">Read</p>
+        </div>
+        <div className="footerItem">
+          <p className="footerValue">{post.views}</p>
+          <p className="footerLabel">Views</p>
+        </div>
+        <div className="footerItem">
+          <p className="footerValue">{post.comments}</p>
+          <p className="footerLabel">Comments</p>
+        </div>
+      </div>
     </div>
   );
 }
 
-export { Post }; 
+export default Post;
